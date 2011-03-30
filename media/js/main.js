093605d8e82a78bfb6ab9cf1998f773294fa2707
Rogerio Bragil <bragil@gmail.com>
2011-03-30 13:16:58 -0300

    Adicionadas as rotinas para preenchimento dos combos de capítulo e versículo.
    Signed-off-by: Rogerio Bragil <bragil@gmail.com>

--- media/js/main.js
+++ media/js/main.js
@@ -1,11 +1,57 @@
 // Rotinas Javascript
+
+function resetaCombo( el )
+{
+	$("select[name='"+el+"']").empty();//retira os elementos antigos
+	var option = document.createElement('option');
+	$( option ).attr( {value : '0'} );
+	$( option ).append( '' );
+	$("select[name='"+el+"']").append( option );
+}
+
 $(function() {
 	$( "#livro_capitulo" ).autocomplete({
 				// Busca os livros e seus respectivos capítulos
 				source: "/livros/",
 				minLength: 2,
 				select: function( event, ui ) {
-					alert(ui.item.value);
+					//alert(ui.item.value);
+					
+					$.getJSON('/get_capitulos/',
+							{l:ui.item.id},
+							function(data) {
+								//data = eval(data);
+								//list = eval('[' + data + ']');
+								var option = new Array();
+								$("#livro_id").val(ui.item.id);
+								
+								resetaCombo('capitulo');
+								$("select[name='capitulo']").attr("disabled", false);
+								$.each(data, function(i, obj) {
+									option[i] = document.createElement('option');//criando o option
+									$( option[i] ).attr( {value : obj} );//colocando o value no option
+									$( option[i] ).append( obj );//colocando o 'label'
+			 
+									$("select[name='capitulo']").append( option[i] );//jogando um à um os options no próximo combo
+								});
+							});
 				}
 			});
+	
+	$("#capitulo").change( function() {
+		$.getJSON('/get_versiculos/?l=' + $("#livro_id").val() + '&c=' + $("#capitulo").val(),
+				function(data) {
+					var option = new Array();
+					
+					resetaCombo('versiculo');
+					$("select[name='versiculo']").attr("disabled", false);
+					$.each(data, function(i, obj) {
+						option[i] = document.createElement('option');//criando o option
+						$( option[i] ).attr( {value : obj} );//colocando o value no option
+						$( option[i] ).append( obj );//colocando o 'label'
+
+						$("select[name='versiculo']").append( option[i] );//jogando um à um os options no próximo combo
+					});
+				});
+	});
 });
